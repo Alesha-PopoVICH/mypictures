8c71802023474457d29a909a6aa95a9a36a2acc8
Alesha-PopoVICH <55570113+Alesha-PopoVICH@users.noreply.github.com>
2019-10-25 10:50:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import turtle 
 
+def square (x, y, size, color)
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.forward()      
+    turtle.left()        
+    turtle.forward()     
+    turtle.left()
+    turtle.forward()      
+    turtle.left()
+    turtle.forward()
+    
+    
