d8eb1eb09ad8c635f4c5c88d9722ff958055c558
Alesha-PopoVICH <55570113+Alesha-PopoVICH@users.noreply.github.com>
2019-11-08 11:28:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import turtle
+import math
+#Арсений делает квадрат
+def square (x, y, size, color, angle)
+turtle.goto(x,y)
+turtle.left(angle)
+turtlr.pendown()
+turtle.left(angle)
+turtle.forward(size)
+turtle.left(90)
+turtle.forward(size)
+turtle.left(90)
+turtle.forward(size)
+turtle.left(90)
+turtle.forward(size)
+turtle.left(90)
+turtle.penup()
+pass
+
+
+
+
 
