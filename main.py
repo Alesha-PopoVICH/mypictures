3edd9636d85fafb31c10c36012fc2eddb68bf486
Alesha-PopoVICH <55570113+Alesha-PopoVICH@users.noreply.github.com>
2019-12-06 10:33:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,5 +21,77 @@ pass
 
 
 
+turtle.penup()
+turtle.goto(35, 35)
+turtle.right(90) # Повернуть курсор на 90 градусов вправо
+turtle.pendown()
+turtle.forward(70) # Пройти вперед расстояние 70, если курсор опущен, то будет нарисована линия по пути слоедования
+
+turtle.right(90)
+turtle.forward(70)
+
+turtle.right(90)
+turtle.forward(70)
+
+turtle.right(90)
+turtle.forward(70)
+
+turtle.penup()
+turtle.goto(45, 35)
+turtle.right(225)
+turtle.pendown()
+turtle.forward(70)
+
+turtle.left(90)
+turtle.forward(70)
+
+turtle.left(135) # Повернуть курсор влево на 135 градусов
+turtle.forward(100)
+
+turtle.done()
+
+
+turtle.color("blue") # Устанавливаем цвет черепашки
+turtle.penup() # Поднимаем курсор
+turtle.goto(-110, -25) # Переходим по нужным координатам
+turtle.pendown() # Опускаем курсор
+turtle.circle(45) # Рисуем круг с радиусом 45
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(0, -25)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("red")
+turtle.penup()
+turtle.goto(110, -25)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("yellow")
+turtle.penup()
+turtle.goto(-55, -75)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("green")
+turtle.penup()
+turtle.goto(55, -75)
+turtle.pendown()
+turtle.circle(45)
+
+turtle.color("black")
+turtle.penup()
+turtle.goto(0, 80)
+turtle.pendown()
+turtle.write("Olympic Symbol") # Вместо еще одного круга выводим надпись "Olympic Symbol"
+
+turtle.done()
+
+
+
+
+
 
 
